529bf68be6c5157184a1cab4770f52a39edbead6
Marcel Körtgen <marcel.koertgen@gmail.com>
2019-01-02 16:45:27 +0100

    FIx docker build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 VCS_REF=`git rev-parse --short HEAD`
 BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 # override DOCKER_TAG with TRAVIS_TAG if set
-export DOCKER_TAG=${TRAVIS_TAG:-9.2.5}
+DOCKER_TAG=${TRAVIS_TAG:-0.1.0}
 docker-compose build
